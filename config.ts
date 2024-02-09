72f1ad9605283c909daeec8ac9b8be9529d9da8e
Anirudh Chowdary <90593459+AnirudhChowdary@users.noreply.github.com>
2024-02-09 13:34:25 -0600

    initial commit with files

---
+++ config.ts
@@ -0,0 +1,71 @@
+export default {
+  // LOGIN DETAILS
+  LINKEDIN_EMAIL: "anirudhchowdaryjava31@gmail.com",
+  LINKEDIN_PASSWORD: "Iron_man@1124",
+
+  // JOB SEARCH PARAMETERS
+  KEYWORDS: "full stack developer",
+  LOCATION: "United States",
+  WORKPLACE: {
+    REMOTE: true,
+    ON_SITE: true,
+    HYBRID: true,
+  },
+  JOB_TITLE: "(Sr.|UI Developer|Senior|Java|java|Angular|J2Ee|angular|react|React|backend|back-end|Javascript|frontend|front-end|Full Stack|full stack|full-stack|nodejs|node|js).*(developer|engineer)",
+  JOB_DESCRIPTION: "^((?!(primeit))(.|[\n\r]))*$",
+  JOB_DESCRIPTION_LANGUAGES: ["english"], // replace value with ["any"] to accept all job description laguages
+
+  // FORM DATA
+  PHONE: "4698440553",
+  CV_PATH: "",
+  COVER_LETTER_PATH: "",
+  HOME_CITY: "",
+  YEARS_OF_EXPERIENCE: {
+    "angular": 6,
+    "react.js": 6,
+    ".net": 3,
+    "php": 7,
+    "spring": 8,
+    "java": 9,
+    "magento": 5,
+    "node": 5,
+    "javascript": 8,
+    "mongodb": 5,
+    "kubernetes": 6,
+    "CI/CD": 6,
+    "python": 5,
+    "drupal": 5,
+    "sass": 6,
+    "html": 9,
+    "google cloud": 5,
+    "docker": 5,
+    "terraform": 5,
+    "css": 4,
+    "typescript": 6,
+    "webmethods": 5,
+    "aws":6,
+    "Microsoft Power BI" : 4,
+    "analyst experience" : 8,
+    "Mainframe" : 6,
+    "COBOL" : 4,
+    "kafka" : 6,
+    "Hadoop" : 8,
+    "DevOps" : 6,
+    "OpenShift" : 5,
+    "Amazon Web Services" : 6
+  },
+  LANGUAGE_PROFICIENCY: {
+    "english": "professional",
+  },
+  REQUIRES_VISA_SPONSORSHIP: false,
+  TEXT_FIELDS: {  "pay": "70" , "experience" : "5" , "w2":"No" },
+  BOOLEANS: {
+    "bachelor": true,
+    "authorized": true,
+    "w2": false,
+  },
+  MULTIPLE_CHOICE_FIELDS: { "w2": "No" },
+
+  // OTHER SETTINGS
+  SINGLE_PAGE: false,
+}
---
+++ utils/buildUrl.ts
@@ -0,0 +1,15 @@
+const buildUrl = (base: string, searchParams: { [x: string]: string }) => {
+  const url = new URL(base);
+
+  for(const key in searchParams) {
+    url.searchParams.append(key, searchParams[key]);
+  }
+
+  url.searchParams.append("f_TPR","r86400");
+  url.searchParams.append("f_JT","C");
+//  url.searchParams.append("sortBy","DD");
+
+  return url;
+};
+
+export default buildUrl;
